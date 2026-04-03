de99163084b21c984e1db937548bc6416fd75ee0
Grover Rocha <grovercarlosr@gmail.com>
2026-04-03 01:51:43 -0500

    WorkoutBuilderUiTest implemented

--- app/src/androidTest/java/com/example/exergen/presentation/WorkoutBuilderUiTest.java
+++ app/src/androidTest/java/com/example/exergen/presentation/WorkoutBuilderUiTest.java
@@ -19,6 +19,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 @RunWith(AndroidJUnit4.class)
 public class WorkoutBuilderUiTest {
 
@@ -46,4 +47,28 @@ public class WorkoutBuilderUiTest {
         onView(withText(containsString("Chest"))).check(matches(isDisplayed()));
     }
 
+
+    @Test
+    public void testWorkoutBuilder_WithEquipmentConstraints() {
+        onView(withId(R.id.nav_add)).perform(click());
+
+        // Fill in details
+        onView(withId(R.id.et_exercise_count)).perform(typeText("2"), closeSoftKeyboard());
+        
+        // Select muscle groups
+        onView(withId(R.id.btn_muscle_toggle)).perform(click());
+        onView(withText("Legs")).perform(click());
+
+        // Select equipment
+        onView(withId(R.id.btn_equipment_toggle)).perform(click());
+        onView(withText("Dumbbells")).perform(click());
+
+        // Generate
+        onView(withId(R.id.btn_generate_workout)).perform(click());
+
+        // Verify results
+        onView(withText("Your Generated Routine")).check(matches(isDisplayed()));
+        onView(withText(containsString("Legs"))).check(matches(isDisplayed()));
+        onView(withText(containsString("Dumbbells"))).check(matches(isDisplayed()));
+    }
 }
